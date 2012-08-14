189e0b71a1c05aa6e81c15dc152f9a7936459c7b
slepher <slepher.chen@taodinet.com>
2012-08-14 14:08:04 +0800

    add method_on_nil exception

--- src/luerl_lib.erl
+++ src/luerl_lib.erl
@@ -93,6 +93,8 @@ setnth(N, V, [E|Es]) -> [E|setnth(N-1, V, Es)].
 
 format_error({badarg,Where,As}) ->
     io_lib:format("badarg in ~w: ~w", [Where,As]);
+format_error({method_on_nil, Key}) ->
+    io_lib:format("undefined method ~w on nil", [Key]);
 format_error({illegal_key,Tab,Key}) ->
     io_lib:format("invalid key in ~w: ~w", [Tab,Key]);
 format_error({illegal_index,Where,I}) ->
