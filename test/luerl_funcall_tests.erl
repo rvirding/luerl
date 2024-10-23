9e332b69dabe318879065158eeb1f6f35e88fb2b
Dave Lucia <davelucianyc@gmail.com>
2024-10-22 21:31:37 -0400

    all tests passing

--- test/luerl_funcall_tests.erl
+++ test/luerl_funcall_tests.erl
@@ -67,11 +67,11 @@ define_fun_in_lua_test() ->
 	      "  end\n"
 	      "end\n">>,
     {_, State1} = luerl:do(Chunk, State),
-    {[Fun], _State2} = luerl:call_function([mkadder], [1], State1),
-    {[Fun2], _State3} = luerl:call_function([mkadder], [2], State1),
-    ?assertEqual([5], Fun([4], State1)),
-    ?assertEqual([5.0], Fun([4.0], State1)),
-    ?assertEqual([6], Fun2([4], State1)).
+    {[Fun], State2} = luerl:call_function([mkadder], [1], State1),
+    {[Fun2], State3} = luerl:call_function([mkadder], [2], State2),
+    ?assertMatch({[5], _State}, Fun([4], State3)),
+    ?assertMatch({[5.0], _State}, Fun([4.0], State3)),
+    ?assertMatch({[6], _State}, Fun2([4], State3)).
 
 define_fun2_in_lua_test() ->
     State = luerl:init(),
@@ -85,15 +85,14 @@ define_fun2_in_lua_test() ->
 	      "  end\n"
 	      "end\n">>,
     {_, State1} = luerl:do(Chunk, State),
-    {[Fun], _State2} = luerl:call_function([mklist], [5], State1),
-    {[Fun2], _State3} = luerl:call_function([mklist], [10], State1),
-    ?assertEqual([[{1,4}, {2,4}, {3,4}, {4,4}, {5,4}]],
-		 Fun([4], State1)),
-    ?assertEqual([[{1,4.0}, {2,4.0}, {3,4.0}, {4,4.0}, {5,4.0}]],
-		 Fun([4.0], State1)),
-    ?assertEqual([[{1,4}, {2,4}, {3,4}, {4,4}, {5,4},
-		   {6,4}, {7,4}, {8,4}, {9,4}, {10,4}]],
-		 Fun2([4], State1)).
+    {[Fun], State2} = luerl:call_function([mklist], [5], State1),
+    {[Fun2], State3} = luerl:call_function([mklist], [10], State2),
+    ?assertMatch({[[{1,4}, {2,4}, {3,4}, {4,4}, {5,4}]], _State},
+                 Fun([4], State3)),
+    ?assertMatch({[[{1,4.0}, {2,4.0}, {3,4.0}, {4,4.0}, {5,4.0}]], _State},
+                 Fun([4.0], State3)),
+    ?assertMatch({[[{1,4}, {2,4}, {3,4}, {4,4}, {5,4}, {6,4}, {7,4}, {8,4}, {9,4}, {10,4}]], _State},
+                 Fun2([4], State3)).
 
 newindex_metamethod_test() ->
     State = luerl:init(),
